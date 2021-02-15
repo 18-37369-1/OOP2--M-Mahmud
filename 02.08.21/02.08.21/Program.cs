4bbb60f2e966167d3509d63db35e9ea57934ea34
Mahmudul Hasan <mahmudulhasan210999@gmail.com>
2021-02-15 12:47:26 +0600

    Array1D

---
+++ 02.08.21/02.08.21/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._08._21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5, y = 3, z;
+            Console.WriteLine("Numbers are : "+x+"," +y);
+            Console.WriteLine("Numbers are : {0},{1}", x, y);
+            Console.WriteLine("The addition of x and y is {0}", x + y);
+            Console.WriteLine("The subtraction of x and y is {0}", x - y);
+            Console.WriteLine("The multiplication of x and y is {0}", x * y);
+            Console.WriteLine("The division of x and y is {0}", x / y);
+
+            string a = Console.ReadLine();
+            z = Int32.Parse(a);
+            Console.WriteLine("The inserted number is " + z);
+
+            //int i, n, sum = 0;
+
+            //Console.WriteLine("Input number of terms : ");
+            //n = 
+            //n = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("\nThe even numbers are :");
+            //for (i = 1; i <= n; i++)
+            //{
+            //Console.Write("{0} ", 2 * i);
+            //sum += 2 * i;
+            //}
+            //Console.Write("The sum of even numbers : {1} ", n, sum);
+        }
+    }
+}
---
+++ Array1D/Array1D/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array1D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n, l=50;
+            int[] a = new int[100];
+
+            Console.WriteLine("Input the number of elements to store in the array :");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Input {0} elements :", n);
+            for (i = 0; i < n; i++)
+            {
+                Console.Write("element {0} = ", i);
+                a[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("The values store into the array are : ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(a[i]+" ");
+            }
+
+            Console.WriteLine("\nThe values in reverse are : ");
+            for (i = n - 1; i >= 0; i--)
+            {
+                Console.Write(a[i]+" ");
+            }
+            Console.WriteLine("\n");
+
+            // Q2 :
+            int sum=0;
+            for (int x=0;x<n;x++)
+            {
+                sum = sum + a[x];
+            }
+            Console.WriteLine("The summation of the array : {0}",sum);
+
+            // Q3 :
+            int[] b = new int[a.Length];
+            Console.Write("The copied array is : ");
+            for (int x = 0; x < n; x++)
+            {
+                b[x] = a[x];
+                Console.Write(b[x]+" ");
+            }
+            Console.WriteLine("\n");
+
+        }
+    }
+}
